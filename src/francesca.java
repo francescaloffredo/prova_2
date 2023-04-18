6f51083d34ff4feae71fd9a32964d26074bf52a6
francescaloffredo <francesca.loffredo@studenti.univr.it>
2023-04-18 12:33:03 +0200

    ho creato questa classe

--- src/francesca.java
+++ src/francesca.java
@@ -1,2 +1,16 @@
 public class francesca {
+
+    private String name;
+    private String surname;
+    private int age;
+
+    public francesca(String n, String s, int a){
+        this.name = n;
+        this.surname = s;
+        this.age = a;
+    }
+
+    public String toString(){
+        return name + " " + surname + " " + age;
+    }
 }
